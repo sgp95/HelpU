5d8b160d941910c473a7a4206a3afb5d5eeb616b
Hiraoka <santiago95gp@gmail.com>
2017-06-23 14:28:03 -0500

    login con firebase listo. error: no reconoce usuario activo

--- app/src/main/java/com/sgp95/santiago/helpu/LoginActivity.java
+++ app/src/main/java/com/sgp95/santiago/helpu/LoginActivity.java
@@ -1,18 +1,19 @@
 package com.sgp95.santiago.helpu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.sgp95.santiago.helpu.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import static com.sgp95.santiago.helpu.R.id.btn_login;
 
@@ -20,19 +21,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText edtLoginCode, edtLoginPass;
     private Button btnLogin,btnPush;
-    private DatabaseReference databaseReference;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            finish();
+        }
+
+        setContentView(R.layout.activity_login);
 
         edtLoginCode = (EditText)findViewById(R.id.txt_login_code);
         edtLoginPass = (EditText)findViewById(R.id.txt_login_password);
         btnLogin = (Button)findViewById(btn_login);
         btnPush = (Button)findViewById(R.id.btn_push_user);
+
+        auth = FirebaseAuth.getInstance();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,41 +59,31 @@ public class LoginActivity extends AppCompatActivity {
 
     public void loginUser(){
         String code = edtLoginCode.getText().toString();
-
-        databaseReference.child("users").orderByChild("userCode").startAt(code).endAt(code).addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                //Este log es para probar si obtengo el user
-                Log.d("LoginActivity","user found: "+dataSnapshot.toString());
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.w("LoginActivity","LoginError",databaseError.toException());
-            }
-        });
-    }
-    public void pushUser(){
-        String code = edtLoginCode.getText().toString();
         String pass = edtLoginPass.getText().toString();
 
-        User user = new User(code,pass);
+        if(TextUtils.isEmpty(code)){
+            Toast.makeText(getApplicationContext(),"Ingrese correo ejmplo: ****@utp.edu.pe",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pass)){
+            Toast.makeText(getApplicationContext(),"Ingrese contraseña",Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        databaseReference.child("users").setValue(user);
+        auth.signInWithEmailAndPassword(code,pass)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login fallido verifique correo y contraseña",Toast.LENGTH_SHORT).show();
+                        }else {
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+    public void pushUser(){
     }
 }
--- app/src/main/java/com/sgp95/santiago/helpu/MainActivity.java
+++ app/src/main/java/com/sgp95/santiago/helpu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sgp95.santiago.helpu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -11,16 +12,24 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private  CommentsFragment commentsFragment;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -43,7 +52,17 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("MainActivity","Option Selected: "+item.getTitle());
                         break;
                     case R.id.opt_exit:
-                        Log.d("MainActivity","Option Selected: "+item.getTitle());
+                        auth.signOut();
+                        authStateListener = new FirebaseAuth.AuthStateListener() {
+                            @Override
+                            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                                if(user == null){
+                                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                                    finish();
+                                }
+                            }
+                        };
+                        //Log.d("MainActivity","Option Selected: "+item.getTitle());
                         break;
                 }
                 drawerLayout.closeDrawer(GravityCompat.START);
